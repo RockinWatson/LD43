0125baf24fb6908d95229f2c948ba666752498fa
Joshua Watson <josh.m.watson@gmail.com>
2018-12-01 11:59:07 -0600

    Elf Spawning and basic movement

---
+++ Assets/Scripts/Elf.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class Elf : MonoBehaviour {
+
+    [HideInInspector]
+    public GameObject ElfExit;
+    public float Speed;
+
+    private bool _moveRight = true;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (_moveRight)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * Speed);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * Speed);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "LeftWall")
+        {
+            _moveRight = true;
+        }
+        if (collision.gameObject.tag == "RightWall")
+        {
+            _moveRight = false;
+        }
+        if (collision.gameObject.tag == "Ground")
+        {
+            _moveRight = SetRandLefRight(0.5f);
+        }
+        if (collision.gameObject.tag == "Elf")
+        {
+            if (_moveRight)
+            {
+                _moveRight = false;
+            }
+            else
+            {
+                _moveRight = true;
+            }
+        }
+        if (collision.gameObject.tag == "ElfExit")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private bool SetRandLefRight(float chanceOfSuccess)
+    {
+        return Random.value < chanceOfSuccess;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject[] Objects;
+    public int ObjectPoolSize;
+    public float WaitForNextMax;
+    public float WaitForNextMin;
+    public float CountDown;
+
+    public GameObject ElfExit;
+
+    private List<GameObject> _objectsPool;
+    private Transform _spawnerTrans;
+
+	// Use this for initialization
+	void Start () {
+        _spawnerTrans = GetComponent<Transform>();
+        _objectsPool = new List<GameObject>();
+        foreach (var obj in Objects)
+        {
+            for (int i = 0; i < ObjectPoolSize; i++)
+            {
+                GameObject o = Instantiate(obj);
+                o.SetActive(false);
+                Elf elfScript = o.GetComponent<Elf>();
+                elfScript.ElfExit = ElfExit;
+                _objectsPool.Add(o);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountDown -= Time.deltaTime;
+        if (CountDown <= 0)
+        {
+            SpawnRandObj(_objectsPool, _spawnerTrans);
+            CountDown = Random.Range(WaitForNextMin, WaitForNextMax);
+        }
+	}
+
+    private void SpawnRandObj(List<GameObject> objPool, Transform transform) {
+        if (objPool != null)
+        {
+            GameObject gameObj = objPool[Random.Range(0, objPool.Count)];
+            if (!gameObj.activeInHierarchy)
+            {
+                InstantiateObject(gameObj, transform);
+            }
+        }
+    }
+
+    private void InstantiateObject(GameObject gameObj, Transform transform){
+        Vector2 pos;
+        pos = new Vector2(transform.position.x, transform.position.y);
+        gameObj.transform.position = pos;
+        gameObj.SetActive(true);
+    }
+}
