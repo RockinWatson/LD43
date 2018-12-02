e982a836aa652eff5704e3ae296f43c4028aca23
Greg <halfskye@gmail.com>
2018-12-02 15:16:23 -0800

    Scripts: Trap: Launcher: Launch behavior

--- Assets/Scripts/Traps/Launcher.cs
+++ Assets/Scripts/Traps/Launcher.cs
@@ -4,13 +4,51 @@ using UnityEngine;
 
 public class Launcher : Trap {
 
-	// Use this for initialization
-	void Start () {
+    [SerializeField]
+    private float _upwardForce = 20.0f;
+
+    private BoxCollider2D _collider = null;
+    private Collider2D[] _colliders = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _collider = this.GetComponent<BoxCollider2D>();
+        _colliders = new Collider2D[20];
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+    }
+
+    protected override void Activate()
+    {
+        base.Activate();
+
+        this.tag = "Untagged";
+    }
+
+    public void Launch()
+    {
+        //@TODO: Set upward force on nearby Elves
+        int count = _collider.GetContacts(_colliders);
+        if(count > 0)
+        {
+            for(int i = 0; i < count; ++i)
+            {
+                if(_colliders[i].tag == "Elf")
+                {
+                    Rigidbody2D rb = _colliders[i].GetComponent<Rigidbody2D>();
+                    rb.AddForce(Vector2.up * _upwardForce, ForceMode2D.Impulse);
+                }
+            }
+        }
+    }
 }
