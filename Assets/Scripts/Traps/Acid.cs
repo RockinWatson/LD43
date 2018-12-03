84903b713dc1a68fa01f3fc6006b91b2ce9fa327
Greg <halfskye@gmail.com>
2018-12-02 18:17:16 -0800

    Script: Trap: Acid: Basic poison functionality

--- Assets/Scripts/Traps/Acid.cs
+++ Assets/Scripts/Traps/Acid.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Acid : Trap {
 
-	// Use this for initialization
-	void Start () {
+    private BoxCollider2D _collider = null;
+    private Collider2D[] _colliders = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _collider = this.GetComponent<BoxCollider2D>();
+        _colliders = new Collider2D[20];
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +24,30 @@ public class Acid : Trap {
 	void Update () {
 		
 	}
+
+    protected override void Activate()
+    {
+        base.Activate();
+
+        this.tag = "Untagged";
+    }
+
+    public void Poison()
+    {
+        if (IsArmed())
+        {
+            int count = _collider.GetContacts(_colliders);
+            if (count > 0)
+            {
+                for (int i = 0; i < count; ++i)
+                {
+                    if (_colliders[i].tag == "Elf")
+                    {
+                        Elf elf = _colliders[i].GetComponent<Elf>();
+                        elf.Poison();
+                    }
+                }
+            }
+        }
+    }
 }
