1508867d9c906cb9df8209e9b40e1e089737f6ad
Joshua Watson <josh.m.watson@gmail.com>
2018-12-03 12:12:46 -0600

    Added Wobble script and spacebar to start

---
+++ Assets/Scripts/Wobble.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour {
+
+    private Vector2 _floatY;
+    private float _originalY;
+    public float FloatStrength;
+    public float FloatSpeed;
+
+    void Start()
+    {
+        _originalY = transform.position.y;
+    }
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, _originalY + ((float)Math.Sin(Time.time * FloatSpeed) * FloatStrength), transform.position.z);
+    }
+}
