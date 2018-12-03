47eb35059135e471ac759c8bf6e075f9e8ad8746
Joshua Watson <josh.m.watson@gmail.com>
2018-12-02 18:55:56 -0600

    Added Elf Escaped Text and "Score" to UI

---
+++ Assets/Scripts/ElfsEscaped.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ElfsEscaped : MonoBehaviour {
+
+    private TextMesh _scoreGui;
+
+    // Use this for initialization
+    void Start()
+    {
+        _scoreGui = gameObject.GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _scoreGui.text = LevelController.Get().GetEscapedElfs().ToString();
+    }
+}
