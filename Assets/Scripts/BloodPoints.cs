9c5ac0fb22ecd5332ab644c4d04574664fa1e001
Joshua Watson <josh.m.watson@gmail.com>
2018-12-03 18:36:32 -0600

    Added BloodPoints to UI

---
+++ Assets/Scripts/BloodPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodPoints : MonoBehaviour {
+
+    private TextMesh _scoreGui;
+
+    // Use this for initialization
+    void Start()
+    {
+        _scoreGui = gameObject.GetComponent<TextMesh>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _scoreGui.text = Player.Get().GetBloodLevel().ToString();
+    }
+}
